66818b96dd138e3566fb10cc2ce832e931453201
mtohernandez <m4teoh3rnandez@outlook.com>
2022-04-28 15:14:37 -0500

    changed to abuelo

--- src/containers/project_table/ProjectTable.jsx
+++ src/containers/project_table/ProjectTable.jsx
@@ -5,7 +5,7 @@ const ProjectTable = () => {
   return (
       <div className='container'>
         <div className='project_text'>
-            <p>Los proyectos de mi primo</p>
+            <p>Los proyectos de mi abuelo</p>
         </div>
         <div className='project_table_container'>
             <table className='projects_table' border=''>
